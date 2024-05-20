6cb1a8689e695d8c65d0d80b4030213f552cedf0
farkmu45 <farukmicros@outlook.com>
2024-05-20 10:08:51 +0800

    Fix markup

--- resources/views/filament/user/widgets/article-widget.blade.php
+++ resources/views/filament/user/widgets/article-widget.blade.php
@@ -5,36 +5,36 @@
 
   <x-filament::section style="margin-top: 2rem">
     <a href="#" style="display: block">
-      <h3 class="text-2xl font-bold">What is binge eating disorder</h2>
-        <p class="text-sm" style="margin-top: .5rem">
-          Binge Eating Disorder (BED) has been discussed as a disordered eating behavior since the 1950’s but was not
-          officially recognized as its own diagnosis until the Diagnostic and Statistical Manual of Mental Disorders
-          published its 5th Edition (DSM-5) in 2013. Even so, this is not an indicator of the severity or prevalence of
-          BED throughout history. In fact, BED disorder is the most common eating disorder in the United States,
-          therefore, awareness of signs, symptoms, and treatment interventions is important.....
-        </p>
+      <h3 class="text-2xl font-bold">What is binge eating disorder</h3>
+      <p class="text-sm" style="margin-top: .5rem">
+        Binge Eating Disorder (BED) has been discussed as a disordered eating behavior since the 1950’s but was not
+        officially recognized as its own diagnosis until the Diagnostic and Statistical Manual of Mental Disorders
+        published its 5th Edition (DSM-5) in 2013. Even so, this is not an indicator of the severity or prevalence of
+        BED throughout history. In fact, BED disorder is the most common eating disorder in the United States,
+        therefore, awareness of signs, symptoms, and treatment interventions is important.....
+      </p>
     </a>
     <hr style="margin-top: 2rem">
     <a href="#" style="margin-top: 2rem; display: block">
-      <h3 class="text-2xl font-bold">What is binge eating disorder</h2>
-        <p class="text-sm" style="margin-top: .5rem">
-          Binge Eating Disorder (BED) has been discussed as a disordered eating behavior since the 1950’s but was not
-          officially recognized as its own diagnosis until the Diagnostic and Statistical Manual of Mental Disorders
-          published its 5th Edition (DSM-5) in 2013. Even so, this is not an indicator of the severity or prevalence of
-          BED throughout history. In fact, BED disorder is the most common eating disorder in the United States,
-          therefore, awareness of signs, symptoms, and treatment interventions is important.....
-        </p>
+      <h3 class="text-2xl font-bold">What is binge eating disorder</h3>
+      <p class="text-sm" style="margin-top: .5rem">
+        Binge Eating Disorder (BED) has been discussed as a disordered eating behavior since the 1950’s but was not
+        officially recognized as its own diagnosis until the Diagnostic and Statistical Manual of Mental Disorders
+        published its 5th Edition (DSM-5) in 2013. Even so, this is not an indicator of the severity or prevalence of
+        BED throughout history. In fact, BED disorder is the most common eating disorder in the United States,
+        therefore, awareness of signs, symptoms, and treatment interventions is important.....
+      </p>
     </a>
     <hr style="margin-top: 2rem">
     <a href="#" style="margin-top: 2rem; display: block">
-      <h3 class="text-2xl font-bold">What is binge eating disorder</h2>
-        <p class="text-sm" style="margin-top: .5rem">
-          Binge Eating Disorder (BED) has been discussed as a disordered eating behavior since the 1950’s but was not
-          officially recognized as its own diagnosis until the Diagnostic and Statistical Manual of Mental Disorders
-          published its 5th Edition (DSM-5) in 2013. Even so, this is not an indicator of the severity or prevalence of
-          BED throughout history. In fact, BED disorder is the most common eating disorder in the United States,
-          therefore, awareness of signs, symptoms, and treatment interventions is important.....
-        </p>
+      <h3 class="text-2xl font-bold">What is binge eating disorder</h3>
+      <p class="text-sm" style="margin-top: .5rem">
+        Binge Eating Disorder (BED) has been discussed as a disordered eating behavior since the 1950’s but was not
+        officially recognized as its own diagnosis until the Diagnostic and Statistical Manual of Mental Disorders
+        published its 5th Edition (DSM-5) in 2013. Even so, this is not an indicator of the severity or prevalence of
+        BED throughout history. In fact, BED disorder is the most common eating disorder in the United States,
+        therefore, awareness of signs, symptoms, and treatment interventions is important.....
+      </p>
     </a>
     <hr style="margin-top: 2rem">
     <a href="#" style="margin-top: 2rem; display: block">
